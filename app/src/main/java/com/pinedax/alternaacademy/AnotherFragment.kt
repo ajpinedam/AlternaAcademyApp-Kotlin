9f198892c70cc3606d912a4ce5b4135a412843fb
Andres Pineda <pineda.andres@gmail.com>
2022-07-15 08:57:47 -0700

    Initial Commit File -> New App

---
+++ app/src/main/java/com/pinedax/alternaacademy/AnotherFragment.kt
@@ -0,0 +1,13 @@
+package com.pinedax.alternaacademy
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.pinedax.alternaacademy.databinding.FragmentFirstBinding
+
+class AnotherFragment : Fragment() {
+
+}
